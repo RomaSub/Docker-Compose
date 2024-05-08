a05497c01b4293be9810ee3a7c496e4b5e10bf5c
RomaSub <antonfrolov1212@gmail.com>
2024-05-08 10:43:16 +0300

    test

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-hehehe
+h[![Netlify Status](https://api.netlify.com/api/v1/badges/6c0aaba5-602e-4505-9758-47dc1bf7dff6/deploy-status)](https://app.netlify.com/sites/test-chat-test/deploys)ehehe
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-  const a = ;
-  const nums: number[] = []
-  nums.push('')
+type Todo = {
+  text: string;
+  id: number;
+};
+
+type TodoListProps = {
+  todos: Todo[];
+  removeTodo: (id: number) => void;
+};
+
+const TodoList = ({ todos, removeTodo }: TodoListProps) => {
+  return (
+    <ul>
+      {todos.map((el) => (
+        <li key={el.id}>
+          {el.text}
+          <button onClick={() => removeTodo(el.id)}>X</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+const App = () => {
+  const [todos, setTodos] = useState([] as Todo[]);
+  const [text, setText] = useState('' as string);
+
+  const addTodo = () => {
+    if (text === '') return;
+    const id = Date.now();
+    const newTodo = { text, id };
+    setTodos([...todos, newTodo]);
+    setText('');
+  };
+
+  const removeTodo = (id: number) => {
+    const update = todos.filter((el) => el.id !== id);
+    setTodos(update);
+  };
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+      <TodoList todos={todos} removeTodo={removeTodo} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
